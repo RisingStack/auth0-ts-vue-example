28389c365219563f176e82bfafb6387c8963e5a1
Shadowbeetle <tamas.kadlecsik@gmail.com>
2020-07-14 16:02:50 +0200

    remove exclamation points

--- src/auth/User.ts
+++ src/auth/User.ts
@@ -1,13 +1,13 @@
 import { camelCase } from 'lodash'
 
 export class User {
-  sub!: string
-  names!: string
-  nickname!: string
-  picture!: string
-  updatedAt!: string
-  email!: string
-  emailVerified!: boolean
+  sub: string
+  name: string
+  nickname: string
+  picture: string
+  updatedAt: string
+  email: string
+  emailVerified: boolean
 
   provider?: string
   id?: string
@@ -24,7 +24,7 @@ export class User {
     }
 
     this.sub = auth0User.sub as string
-    this.provider = this.sub?.split('|')[0]
-    this.id = this.sub?.split('|')[1]
+    this.provider = this.sub.split('|')[0]
+    this.id = this.sub.split('|')[1]
   }
 }
